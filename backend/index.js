9d492e67be8ca6f394e124fd774673e8514aa033
Kzzisung <gitkjsjanoneight@gmail.com>
2026-06-08 03:14:32 +0900

    frontend 경로 수정

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,9 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'frontend')));
-
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 // PostgreSQL 연결 풀
